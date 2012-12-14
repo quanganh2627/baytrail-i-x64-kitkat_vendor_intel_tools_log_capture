4d4ad3a0f57263d4da49b6e2147f3d3c1f387b79
Edward Marmounier <edward.marmounier@intel.com>
2012-12-14 10:30:45 +0100

    [PORT FROM JBMR0] Amtl: link to MMGR java lib as static
    BZ: 74736
    
    The MMGR java lib is now built as static.
    The Amtl app must be linked against MMGR as static
    instead of shared / dynamic.
    
    Change-Id: I3c02e1f70cfd3fb43b013e264ed979c892d86a94
    Orig-Change-Id: I44d41c7fa1253956fdaa9190408854cdb71d709b
    Signed-off-by: Edward Marmounier <edward.marmounier@intel.com>
    Reviewed-on: http://android.intel.com:8080/84514
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- amtl/Android.mk
+++ amtl/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_JAVA_LIBRARIES := com.intel.internal.telephony.MmgrClient
+LOCAL_STATIC_JAVA_LIBRARIES := com.intel.internal.telephony.MmgrClient
 
 # Only compile source java files in this apk.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
