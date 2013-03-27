0c55a94e6e14894218de9f61c2bb58d5581af405
Thiry, JeanX <jeanx.thiry@intel.com>
2013-03-27 18:50:13 +0100

    [PORT FROM MAIN] crashlogd : copy *.lost file to crashlog directories
    BZ: 90338
    
    In order to avoid to detect twice the same dropbox crash when full dropbox
    conditions are encountered, the *.lost dropbox crash files have to be copied to
    crashlog directories. This copy should allow to manage at upper level (by
    phonedoctor) this crash duplication by detecting crashes associated to a same
    dropbox file.
    
    Change-Id: If5d7603cb7b9ec1c33f97a212a45918f50f1e782
    Signed-off-by: Thiry, JeanX <jeanx.thiry@intel.com>
    Reviewed-on: http://android.intel.com:8080/99537
    Reviewed-by: Vidoine, Charles EdouardX <charles.edouardx.vidoine@intel.com>
    Reviewed-by: Benoit, NicolasX <nicolasx.benoit@intel.com>
    Reviewed-by: Rocher, Jeremy <jeremy.rocher@intel.com>
    Reviewed-by: Imougar, JacquesX <jacquesx.imougar@intel.com>
    Tested-by: Imougar, JacquesX <jacquesx.imougar@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    Reviewed-on: http://android.intel.com:8080/102955
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Guerard, Christophe <christophe.guerard@intel.com>

--- crashlog/crashlogd.c
+++ crashlog/crashlogd.c
@@ -2009,15 +2009,16 @@ void process_modem_crash_event(char *filename, char *eventname, char *name,  uns
 * Name          : process_full_dropbox_event
 * Description   : processes anr, javacrash and watchdog full dropbox events
 * Parameters    :
+*   char *filename        -> path of watched directory
 *   char *name            -> name of the file inside the watched directory that has triggered the event
 *   unsigned int files    -> nb max of logs destination directories (crashlog, aplogs, bz... )*/
-void process_full_dropbox_event(char *name,  unsigned int files) {
+void process_full_dropbox_event(char *filename, char *name,  unsigned int files) {
 
     char date_tmp[32];
     char date_tmp_2[32];
     char key[SHA1_DIGEST_LENGTH+1];
     int dir;
-    char destion[PATHMAX];
+    char destion[PATHMAX], path[PATHMAX];
     char lostevent[32] = { '\0', };
     char lostevent_subtype[32] = { '\0', };
 
@@ -2045,7 +2046,10 @@ void process_full_dropbox_event(char *name,  unsigned int files) {
         notify_crashreport();
         return;
     }
-
+    /*copy the *.lost dropbox file*/
+    snprintf(path, sizeof(path),"%s/%s",filename,name);
+    snprintf(destion,sizeof(destion),"%s%d/%s", CRASH_DIR,dir,name);
+    do_copy(path, destion, 0);
     snprintf(destion,sizeof(destion),"%s%d/",CRASH_DIR,dir);
     LOGE("%-8s%-22s%-20s%s %s\n", CRASHEVENT, key, date_tmp_2, lostevent, destion);
     usleep(TIMEOUT_VALUE);
@@ -3009,7 +3013,7 @@ static int file_monitor_handle(unsigned int files)
                     /* for full dropbox */
                     else if(strstr(event->name, wd_array[i].cmp) && (strstr(event->name, ".lost" ))){
 
-                        process_full_dropbox_event(event->name, files);
+                        process_full_dropbox_event(wd_array[i].filename, event->name, files);
                         break;
                     }
                     /* for hprof */
