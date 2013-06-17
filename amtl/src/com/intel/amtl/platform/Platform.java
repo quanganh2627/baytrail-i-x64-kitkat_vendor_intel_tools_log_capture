d7c67d1878573bbfead88a21dc8cb25ab10b9269
Morgane BUTSCHER <morganex.butscher@intel.com>
2013-06-17 09:28:37 +0200

    AMTL: AMTL version 2.0
    BZ: 121518
    
    This is a complete AMTL rework to provide the following features:
    - scalability: cross platform support, based on runtime detection.
    - new user interface: redesigned user interface.
    - dynamic setup: platform reboot to apply configuration is no more required.
    - log backup: aplog and bplog snapshot feature to allow in field backup when log upload is not possible.
    - custom log commands: AMTL allow user define log command.
    - enhanced debug log: AMTL logs all actions in android logcat facility.
    
    Change-Id: I05dd722c88b801911fda04186d60d9debc2e4df6
    Signed-off-by: Morgane BUTSCHER <morganex.butscher@intel.com>
    Signed-off-by: Erwan Bracq <erwan.bracq@intel.com>
    Reviewed-on: http://android.intel.com:8080/118525
    Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
    Reviewed-by: Marmounier, Edward <edward.marmounier@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

---
+++ amtl/src/com/intel/amtl/platform/Platform.java
@@ -0,0 +1,102 @@
+/* Android AMTL
+ *
+ * Copyright (C) Intel 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Morgane Butscher <morganeX.butscher@intel.com>
+ * Author: Erwan Bracq <erwan.bracq@intel.com>
+ */
+package com.intel.amtl.platform;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+
+public class Platform {
+
+    private final String TAG = "AMTL";
+    private final String MODULE = "Platform";
+    private final String CPU_FILE = "/sys/kernel/telephony/cpu_name";
+    private final String MODEM_FILE = "/sys/kernel/telephony/modem_name";
+    private String cpuName = "";
+    private String modemName = "";
+    private String catalogPath = "/system/etc/amtl/catalog/";
+
+    public Platform() {
+        getCpuName();
+        getModemName();
+    }
+
+    public String getPlatformConf() {
+        return catalogPath + cpuName + "_" + modemName + ".cfg";
+    }
+
+    private void getModemName() {
+        InputStream inStream = null;
+        try {
+            inStream = new FileInputStream(MODEM_FILE);
+            if (inStream != null) {
+                byte[] b = new byte[inStream.available()];
+                inStream.read(b);
+                String name = new String(b);
+                this.modemName = name.substring(0, name.indexOf("\n"));
+                Log.d(TAG, MODULE + ": Identified cpu name: " + this.modemName);
+            }
+        } catch (FileNotFoundException ex) {
+            Log.e(TAG, MODULE + ": " + MODEM_FILE + " has not been found" + ex);
+        } catch (IOException ex) {
+            Log.e(TAG, MODULE + ": Unable to read file " + MODEM_FILE + " " + ex);
+        } finally {
+            if (inStream != null) {
+                try {
+                    inStream.close();
+                } catch (IOException ex) {
+                    Log.e(TAG, MODULE + ": Error during close " + ex);
+                }
+            }
+        }
+    }
+
+    private void getCpuName() {
+        InputStream inStream = null;
+        try {
+            inStream = new FileInputStream(CPU_FILE);
+            if (inStream != null) {
+                byte[] b = new byte[inStream.available()];
+                inStream.read(b);
+                String name = new String(b);
+                this.cpuName = name.substring(0, name.indexOf("\n"));
+                Log.d(TAG, MODULE + ": Identified cpu name: " + this.cpuName);
+            }
+        } catch (FileNotFoundException ex) {
+            Log.e(TAG, MODULE + ": " + CPU_FILE + " has not been found" + ex);
+        } catch (IOException ex) {
+            Log.e(TAG, MODULE + ": Unable to read file " + CPU_FILE + " " + ex);
+        } finally {
+            if (inStream != null) {
+                try {
+                    inStream.close();
+                } catch (IOException ex) {
+                    Log.e(TAG, MODULE + ": Error during close " + ex);
+                }
+            }
+        }
+    }
+}
