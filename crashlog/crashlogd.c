87a9c2ee0a4c5f0bb2baec5204fa06c9cf6d5896
Christophe Guerard <christophe.guerard@intel.com>
2013-06-25 15:24:12 +0200

    crashlogd:Add new fabric error type for TNG
    BZ: 118419
    
    Change-Id: I004ae6ecb8548a859936ac3abfafee00f5386f0d
    Signed-off-by: Christophe Guerard <christophe.guerard@intel.com>
    Reviewed-on: http://android.intel.com:8080/115357
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- crashlog/crashlogd.c
+++ crashlog/crashlogd.c
@@ -4148,6 +4148,13 @@ struct fabric_type ft_array[] = {
     {"DW0:", "f502", "INSTERR"},
     {"DW0:", "f504", "SRAMECCERR"},
     {"DW0:", "00dd", "HWWDTLOGERR"},
+    {"DW3:", "0000e101", "MEMERR"},
+    {"DW3:", "0000e102", "INSTERR"},
+    {"DW3:", "0000e104", "SRAMECCERR"},
+    {"DW3:", "0000e107", "SCUWDT"},
+    {"DW3:", "0000e108", "PLLLOCKERR"},
+    {"DW3:", "0000e10a", "KERNELHANG"},
+    {"DW3:", "0000e10b", "CHAABIHANG"},
 };
 struct fabric_type fake[] = {
     {"DW2:", "02608002", "FABRIC_FAKE"},
