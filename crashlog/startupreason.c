9e4c584aec2e0913fb42fa6a95f11d9b28a3e776
Thiry, JeanX <jeanx.thiry@intel.com>
2013-08-05 12:26:31 +0200

    [PORT FROM MAIN] crashlogd: Add new wake source
    BZ: 114119
    
    Update wake sources list matching with crashtool startup resaon. Try
    to keep HWWDT and SWWDT as prefix to help crashtool in categorization.
    
    When startup reason contains WDT,HWWDT or SWWDT:
    it generate a WDT crash event to provide
    aplogs and kernel last_kmsg.
    
    Change-Id: Ide341305dd959f5e8aec01cdc9fefbbfef1c1a15
    Orig-Change-Id: I19cd1829b80de8bd73d09afcd35323d7c200b003
    Signed-off-by: Thiry, JeanX <jeanx.thiry@intel.com>

--- crashlog/startupreason.c
+++ crashlog/startupreason.c
@@ -55,6 +55,38 @@ static void flush_aplog_atboot(char *mode, int dir, const char* ts)
     }
 }
 
+/*
+* Name          : read_startupreason
+* Description   : This function returns the decoded startup reason by reading
+*                 the wake source from the command line. The wake src is translated
+*                 to a crashtool startup reason.
+*                 In case of a HW watchdog, the wake sources are translated to a HWWDT
+*                 List of wake sources :
+*    0x00: WAKE_BATT_INSERT
+*    0x01: WAKE_PWR_BUTTON_PRESS
+*    0x02: WAKE_RTC_TIMER
+*    0x03: WAKE_USB_CHRG_INSERT
+*    0x04: Reserved
+*    0x05: WAKE_REAL_RESET -> COLD_RESET
+*    0x06: WAKE_COLD_BOOT
+*    0x07: WAKE_UNKNOWN
+*    0x08: WAKE_KERNEL_WATCHDOG_RESET -> SWWDT_RESET
+*    0x09: WAKE_SECURITY_WATCHDOG_RESET (Chaabi ou TXE/SEC) -> HWWDT_RESET_SECURITY
+*    0x0A: WAKE_WATCHDOG_COUNTER_EXCEEDED -> WDT_COUNTER_EXCEEDED
+*    0x0B: WAKE_POWER_SUPPLY_DETECTED
+*    0x0C: WAKE_FASTBOOT_BUTTONS_COMBO
+*    0x0D: WAKE_NO_MATCHING_OSIP_ENTRY
+*    0x0E: WAKE_CRITICAL_BATTERY
+*    0x0F: WAKE_INVALID_CHECKSUM
+*    0x10: WAKE_FORCED_RESET
+*    0x11: WAKE_ACDC_CHGR_INSERT
+*    0x12: WAKE_PMIC_WATCHDOG_RESET (PMIC/EC) -> HWWDT_RESET_PMIC
+*    0x13: WAKE_PLATFORM_WATCHDOG_RESET -> HWWDT_RESET_PLATFORM
+*    0x14: WAKE_SC_WATCHDOG_RESET (SCU/PMC) -> HWWDT_RESET_SC
+*
+* Parameters    :
+*   char *startupreason   -> string containing the translated startup reason
+*   */
 void read_startupreason(char *startupreason)
 {
     char cmdline[512] = { '\0', };
@@ -72,13 +104,18 @@ void read_startupreason(char *startupreason)
         "COLD_BOOT",
         "UNKNOWN",
         "SWWDT_RESET",
-        "HWWDT_RESET",
-        "WATCHDOG_COUNTER_EXCEEDED",
+        "HWWDT_RESET_SECURITY",
+        "WDT_COUNTER_EXCEEDED",
         "POWER_SUPPLY_DETECTED",
         "FASTBOOT_BUTTONS_COMBO",
         "NO_MATCHING_OSIP_ENTRY",
         "CRITICAL_BATTERY",
-        "INVALID_CHECKSUM"};
+        "INVALID_CHECKSUM",
+        "FORCED_RESET",
+        "ACDC_CHGR_INSERT",
+        "HWWDT_RESET_PMIC",
+        "HWWDT_RESET_PLATFORM",
+        "HWWDT_RESET_SC"};
 
     strcpy(startupreason, "UNKNOWN");
 
@@ -129,13 +166,25 @@ void read_startupreason(char *startupreason)
     }
 }
 
+
+/**
+ * @brief generate WDT crash event
+ *
+ * If startup reason contains HWWDT or SWWDT, generate a WDT crash event.
+ * Copy aplogs and /proc/last_kmsg files in the event.
+ *
+ * @param reason - string containing the translated startup reason
+ */
 int crashlog_check_startupreason(char *reason) {
     const char *dateshort = get_current_time_short(1);
     char destination[PATHMAX];
     int dir;
     char *key;
 
-    if ( !strstr(reason, "WDT_RESET") || strstr(reason, "FAKE") ) {
+    /* Nothing to do if the reason :
+     *  - doesn't contains "HWWDT" or "SWWDT" or "WDT" */
+    /*  - contains "HWWDT" or "SWWDT" or "WDT" but with 'FAKE' suffix */
+    if ( !( strstr(reason, "HWWDT_") || strstr(reason, "SWWDT_") || strstr(reason, "WDT_") ) || strstr(reason, "FAKE") ) {
         /* Nothing to do */
         return 0;
     }
