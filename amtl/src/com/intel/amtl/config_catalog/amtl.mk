1d003d081a70110f50b3123d6806c1d3c3f8bc5e
Morgane BUTSCHER <morganex.butscher@intel.com>
2013-08-28 12:17:10 +0200

    AMTL: Wrong modem name for config file on ctp7160
    BZ: 134200
    
    Modem name to built config file should be XMM_7160_REV1
    instead of XMM_7160.
    
    Change-Id: Ic4140a59bfee87398be69cc417eb8cac21771bad
    Signed-off-by: Morgane BUTSCHER <morganex.butscher@intel.com>
    Reviewed-on: http://android.intel.com:8080/128243
    Reviewed-by: Bracq, Erwan <erwan.bracq@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- amtl/src/com/intel/amtl/config_catalog/amtl.mk
+++ amtl/src/com/intel/amtl/config_catalog/amtl.mk
@@ -8,6 +8,7 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/CLOVERVIEW_XMM_6268.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_6268.cfg \
         $(LOCAL_PATH)/CLOVERVIEW_XMM_6360.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_6360.cfg \
         $(LOCAL_PATH)/CLOVERVIEW_XMM_7160.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_7160.cfg \
+        $(LOCAL_PATH)/CLOVERVIEW_XMM_7160_REV1.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_7160_REV1.cfg \
         $(LOCAL_PATH)/VALLEYVIEW2_XMM_7160_REV3_5.cfg:system/etc/amtl/catalog/VALLEYVIEW2_XMM_7160_REV3_5.cfg \
         $(LOCAL_PATH)/VALLEYVIEW2_XMM_7160.cfg:system/etc/amtl/catalog/VALLEYVIEW2_XMM_7160.cfg
 
