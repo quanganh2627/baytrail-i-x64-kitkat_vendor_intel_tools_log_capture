646e2ffdf5393f7fc07a2e9a7eefef72677579b4
Morgane BUTSCHER <morganex.butscher@intel.com>
2013-08-30 09:49:19 +0200

    AMTL: Add configuration file for TANGIER XMM_7160_REV4
    BZ: 134601
    
    Add configuration file for TANGIER XMM_7160_REV4
    
    Change-Id: I5b51c9e0cbc0e5458f992ce856eae204f606cf03
    Signed-off-by: Morgane BUTSCHER <morganex.butscher@intel.com>
    Reviewed-on: http://android.intel.com:8080/128734
    Reviewed-by: Bracq, Erwan <erwan.bracq@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- amtl/src/com/intel/amtl/config_catalog/amtl.mk
+++ amtl/src/com/intel/amtl/config_catalog/amtl.mk
@@ -5,6 +5,7 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/TANGIER_XMM_6360.cfg:system/etc/amtl/catalog/TANGIER_XMM_6360.cfg \
         $(LOCAL_PATH)/TANGIER_XMM_7160_REV3.cfg:system/etc/amtl/catalog/TANGIER_XMM_7160_REV3.cfg \
         $(LOCAL_PATH)/TANGIER_XMM_7160_REV3_5.cfg:system/etc/amtl/catalog/TANGIER_XMM_7160_REV3_5.cfg \
+        $(LOCAL_PATH)/TANGIER_XMM_7160_REV4.cfg:system/etc/amtl/catalog/TANGIER_XMM_7160_REV4.cfg \
         $(LOCAL_PATH)/CLOVERVIEW_XMM_6268.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_6268.cfg \
         $(LOCAL_PATH)/CLOVERVIEW_XMM_6360.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_6360.cfg \
         $(LOCAL_PATH)/CLOVERVIEW_XMM_7160.cfg:system/etc/amtl/catalog/CLOVERVIEW_XMM_7160.cfg \
