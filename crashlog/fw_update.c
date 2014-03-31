316d70664c0beb4933817fa077dd95b591a07616
François Trebosc <francoisx.trebosc@intel.com>
2014-03-31 16:11:51 +0200

    Add management of capsule update file for EDK2
    BZ: 184381
    
    On EDK platform update capsule status is reported using efi variable.
    If we aren't be able to get capsule status from osnib file, we try
    to read EDK capsule file.
    
    Change-Id: I03d3058352f7cd4c5239caf8e54689c1ddc38b34
    Signed-off-by: François Trebosc <francoisx.trebosc@intel.com>

--- crashlog/fw_update.c
+++ crashlog/fw_update.c
@@ -23,6 +23,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include "crashutils.h"
+#include "fsutils.h"
+#include "uefivar.h"
 
 #define FW_UPDATE_ERROR "FW_UPDATE_ERROR_errorevent"
 #define FW_UPDATE_NO_STATUS "FW_UPDATE_NO_STATUS"
@@ -30,6 +32,113 @@
 #define PREFIX "Failures detected during firmware update:"
 #define MAX_MSG_STATUS  39
 #define MAX_REASON_SIZE  22
+
+struct fw_update_status_ops {
+    int (*get_fw_update_status)();
+    int (*clear_fw_update_status)(char *msg_reason, char *msg_status);
+};
+
+
+#ifdef CONFIG_UEFI
+
+static int edk2_get_fw_update_status()
+{
+    const char *var_guid_common = "4a67b082-0a4c-41cf-b6c7-440b29bb8c4f";
+    const char *var_capsule_update = "CapsuleUpdateStatus";
+    char resetsource[32] = { '\0', };
+    int fw_update_status = 0;
+    int ret = 0;
+
+    ret = read_resetsource(resetsource);
+    if (ret != 0)
+        return -1;
+
+    if (strcmp("RESET_FW_UPDATE",resetsource) != 0 ) {
+        LOGI("resetsource is %s, no capsule update status to parse", resetsource);
+        return 0;
+    }
+
+    ret = uefivar_get_int(var_capsule_update, var_guid_common, &fw_update_status);
+    if (ret) {
+        LOGE("uefivar_get error %s-%s : %d\n",
+                var_capsule_update, var_guid_common, ret);
+        return -1;
+    }
+
+    return fw_update_status;
+}
+
+static int edk2_clear_fw_update_status(char *msg_reason, char *msg_status)
+{
+    return 0;
+}
+
+static const struct fw_update_status_ops fw_ope = {
+    .get_fw_update_status = edk2_get_fw_update_status,
+    .clear_fw_update_status = edk2_clear_fw_update_status
+};
+
+#else
+
+static int fdk_get_fw_update_status()
+{
+    FILE *filed = NULL;
+    int fw_update_status = 0;
+    // msg_reason reports the reasons for the detected errors
+    char msg_reason[256];
+    char *end;
+
+    // if the sys_fs entry does not exist or fails to read, then exit
+    filed = fopen(FW_UPDATE_STATUS_PATH, "r");
+    if (filed == NULL) {
+        LOGI("Cannot open file: %s\n", FW_UPDATE_STATUS_PATH);
+        return -1;
+    }
+
+    /* update status is within 0..31 range + a new line */
+    errno = 0;
+    fgets(msg_reason, 4, filed);
+    fclose(filed);
+    fw_update_status = strtoul(msg_reason, &end, 10);
+    if (*end != '\n' || errno == ERANGE) {
+        LOGE("failed to parse firmware update value %s %d\n", msg_reason, fw_update_status);
+        raise_infoerror(ERROREVENT, FW_UPDATE_NO_STATUS);
+        return -1;
+    }
+
+    return fw_update_status;
+}
+
+static int fdk_clear_fw_update_status(char *msg_reason, char *msg_status)
+{
+    int ret = 0;
+
+    int fd = open(FW_UPDATE_STATUS_PATH, O_WRONLY);
+    if (fd == -1) {
+        LOGE("failed to open file %s to clear the FW update status flag\n",
+             FW_UPDATE_STATUS_PATH);
+        strncpy(msg_status, "Cannot open sys_fs to clear fw status", MAX_MSG_STATUS);
+        return -1;
+    }
+
+    if (write(fd, msg_reason, 1) == -1) {
+        LOGE("failed to write file %s to clear the FW update status flag\n",
+            FW_UPDATE_STATUS_PATH);
+        strncpy(msg_status, "Cannot write sys_fs to clear fw status", MAX_MSG_STATUS);
+        ret = -1;
+    }
+
+    close(fd);
+    return ret;
+}
+
+static const struct fw_update_status_ops fw_ope = {
+    .get_fw_update_status = fdk_get_fw_update_status,
+    .clear_fw_update_status = fdk_clear_fw_update_status
+};
+
+#endif
+
 
 /**
  * @brief generate fw update crash event
@@ -54,34 +163,21 @@ int crashlog_check_fw_update_status(void)
     char msg_status[MAX_MSG_STATUS+1] = "Successfully cleared fw status";
     // msg_reason reports the reasons for the detected errors
     char msg_reason[256];
-    char *end;
     int fw_update_status;
-    int ret = 0;
     unsigned int i;
-    FILE *filed;
+    int result = 0;
 
-    // if the sys_fs entry does not exist or fails to read, then exit
-    filed = fopen(FW_UPDATE_STATUS_PATH, "r");
-    if (filed == NULL){
-        LOGE("cannot open file: %s\n", FW_UPDATE_STATUS_PATH);
-        return -1;
+    fw_update_status = fw_ope.get_fw_update_status();
+    if (fw_update_status == -1) {
+        return fw_update_status;
     }
 
     /*
      * Log a CRASH event into /logs/history_event file along with the
      * path to the crashfile that is filled later on with the details.
      */
-    errno = 0;
+    LOGI("fw_update_status = %d\n", fw_update_status);
 
-    /* update status is within 0..31 range + a new line */
-    fgets(msg_reason, 4, filed);
-    fclose(filed);
-    fw_update_status = strtoul(msg_reason, &end, 10);
-    if (*end != '\n' || errno == ERANGE) {
-        LOGE("failed to parse firmware update value %s %d\n", msg_reason, fw_update_status);
-        raise_infoerror(ERROREVENT, FW_UPDATE_NO_STATUS);
-        return -1;
-    }
     // if fw_update_status = 0 => no error so nothing to process
     if (!fw_update_status)
         return 0;
@@ -94,23 +190,12 @@ int crashlog_check_fw_update_status(void)
             strncat(msg_reason, ", ", 2);
         }
 
-    // As the fw update status was not zero => clear it
-    int fd = open(FW_UPDATE_STATUS_PATH, O_WRONLY);
-    if (fd == -1) {
-        LOGE("failed to open file %s to clear the FW update status flag\n",
-              FW_UPDATE_STATUS_PATH);
-        strncpy(msg_status, "Cannot open sys_fs to clear fw status", MAX_MSG_STATUS);
-    } else if (write(fd, msg_reason, 1) == -1) {
-        LOGE("failed to write file %s to clear the FW update status flag\n",
-              FW_UPDATE_STATUS_PATH);
-        strncpy(msg_status, "Cannot write sys_fs to clear fw status", MAX_MSG_STATUS);
-        ret = -1;
-    }
-    close(fd);
+    LOGI("msg_reason = %s\n", msg_reason);
+
+    result = fw_ope.clear_fw_update_status(msg_reason, msg_status);
 
     // data0=PREFIX, data1=msg_reason, data2=msg_status
     create_infoevent(FW_UPDATE_ERROR, (char*) PREFIX, msg_reason, msg_status);
 
-    return ret;
+    return result;
 }
-
