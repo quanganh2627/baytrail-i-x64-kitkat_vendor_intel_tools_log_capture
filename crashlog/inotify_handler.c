a35a715f341ae54b918f56a586af3a185bbd0121
Jeremy Rocher <jeremy.rocher@intel.com>
2014-09-18 15:58:45 +0200

    crashlogd: fix event type enum and wd_array definitions
    print_eventtype is realigned with event type enum.
    
    wd_array must be of same size of event type enum in the current design
    as wd_array is walked using event type enum. This is due to bad design
    as it should not have correlation. FULL_REPORT is removed from wd_array
    as a workaround.
    
    Due to this workaround, all folders are always watched as full wd_array
    is initialised. Still, callback function are not set thanks to
    FULL_REPORT if-def in main when setting callbacks, thus no action will
    happen in case of change in those folders.
    
    Change-Id: I6a5f191de4532789ade7579f4b4eaa05866a5b04
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>
    Reviewed-on: https://android.intel.com/275385
    Reviewed-by: Benoit, NicolasX <nicolasx.benoit@intel.com>
    Reviewed-by: Fouassier, Yann <yann.fouassier@intel.com>

--- crashlog/inotify_handler.c
+++ crashlog/inotify_handler.c
@@ -64,17 +64,13 @@ struct watch_entry wd_array[] = {
     {0, DROPBOX_DIR_MASK,   JAVATOMBSTONE_TYPE, 0,  JAVA_TOMBSTONE_EVNAME,   DROPBOX_DIR,   "native_crash",             NULL},
     {0, DROPBOX_DIR_MASK,   JAVACRASH_TYPE, 0,      JAVACRASH_EVNAME,   DROPBOX_DIR,        "app_crash",                NULL},
     {0, DROPBOX_DIR_MASK,   JAVACRASH_TYPE2, 0,     JAVACRASH_EVNAME,   DROPBOX_DIR,        "system_server_crash",      NULL},
-#ifdef FULL_REPORT
     {0, CORE_DIR_MASK,      APCORE_TYPE,    0,      APCORE_EVNAME,      HISTORY_CORE_DIR,   ".core",                    NULL},
     {0, CORE_DIR_MASK,      HPROF_TYPE,     0,      HPROF_EVNAME,       HISTORY_CORE_DIR,   ".hprof",                   NULL},
     {0, STAT_DIR_MASK,      STATTRIG_TYPE,  0,      STATSTRIG_EVNAME,   STAT_DIR,           "_trigger",                 NULL},
     {0, STAT_DIR_MASK,      INFOTRIG_TYPE,  0,      STATSTRIG_EVNAME,   STAT_DIR,           "_infoevent",               NULL},
     {0, STAT_DIR_MASK,      ERRORTRIG_TYPE, 0,      STATSTRIG_EVNAME,   STAT_DIR,           "_errorevent",              NULL},
-#endif
     {0, APLOG_DIR_MASK,     APLOGTRIG_TYPE, 0,      APLOGTRIG_EVNAME,   APLOG_DIR,          "_trigger",                 NULL},
-#ifdef FULL_REPORT
     {0, APLOG_DIR_MASK,     CMDTRIG_TYPE,   0,      CMDTRIG_EVNAME,     APLOG_DIR,          "_cmd",                     NULL},
-#endif
     /* -----------------------------above is dir, below is file------------------------------------------------------------ */
     {0, UPTIME_MASK,        UPTIME_TYPE,    0,      UPTIME_EVNAME,      UPTIME_FILE,        NULL,                      NULL},
     /* -------------------------above is AP, below is modem---------------------------------------------------------------- */
