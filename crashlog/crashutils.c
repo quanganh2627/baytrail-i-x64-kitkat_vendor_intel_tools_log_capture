cd766c50f13ff85f52721d55dee6a2fe50cbbe87
Benoit, NicolasX <nicolasx.benoit@intel.com>
2014-10-03 18:20:23 +0200

    crashlog : launch crashreport notification in a thread
    System call could hang in low memory case.
    Crashreport notifications are now launched in a thread.
    Struct arg_thread added for potential update in cmd
    
    Signed-off-by: Benoit, NicolasX <nicolasx.benoit@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-4032
    Change-Id: Iedefc313a1a24286b24ff05a0f93dace223da01a
    Reviewed-on: https://android.intel.com:443/283652

--- crashlog/crashutils.c
+++ crashlog/crashutils.c
@@ -485,7 +485,7 @@ static char *priv_raise_event(char *event, char *type, char *subtype, char *log,
 
     /* Notify CrashReport except for UIWDT events */
     if (strncmp(type, SYSSERVER_EVNAME, sizeof(SYSSERVER_EVNAME))) {
-            notify_crashreport();
+        notify_crashreport();
     }
     return strdup(key);
 }
@@ -800,7 +800,7 @@ void start_daemon(const char *daemonname) {
     property_set("ctl.start", (char *)daemonname);
 }
 
-void notify_crashreport() {
+void notify_crashreport_thread() {
     char boot_state[PROPERTY_VALUE_MAX];
 
     /* Does current crashlog mode allow notifs to crashreport ?*/
@@ -821,6 +821,16 @@ void notify_crashreport() {
     if (WIFEXITED(status) && WEXITSTATUS(status)) {
         LOGI("notify crash report status: %d.\n", WEXITSTATUS(status));
         return;
+    }
+}
+
+void notify_crashreport() {
+    int ret = 0;
+    pthread_t thread;
+
+    ret = pthread_create(&thread, NULL, (void *)notify_crashreport_thread, NULL);
+    if (ret < 0) {
+        LOGE("%s: notify_crashreport thread error", __FUNCTION__);
     }
 }
 
