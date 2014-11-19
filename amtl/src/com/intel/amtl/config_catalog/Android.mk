7d0588948bacb2b9e765ed20fe4c83a446b97a4b
Petru Muraru <petrux.muraru@intel.com>
2014-11-19 14:57:57 +0100

    amtl: Add Cherrytrail SSIC configuration
    This patch add a specific AMTL configuration to
    handle the BP logging in case IPC SSIC is selected.
    
    As well the patch modify the path of storing the
    bplogs which have been updated into a encrypted
    partition.
    bplogs are now stored under /data/logs/ partition.
    New location is applicable only to IMIN/GMIN.
    
    Change-Id: If374f991f633e5da9afae7e57a80d0a61c6052d6
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-329
    Signed-off-by: Petru Muraru <petrux.muraru@intel.com>
    Signed-off-by: Nicolas LOUIS <nicolasx.louis@intel.com>
    Reviewed-on: https://android.intel.com:443/303010

--- amtl/src/com/intel/amtl/config_catalog/Android.mk
+++ amtl/src/com/intel/amtl/config_catalog/Android.mk
@@ -17,6 +17,14 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := amtl_cherrytrail_XMM_7260_ssic.cfg
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/telephony
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := amtl_clovertrail_XMM_6360.cfg
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
@@ -102,6 +110,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES :=\
     amtl_baytrail_XMM_7160.cfg \
     amtl_cherrytrail_XMM_7260.cfg \
+    amtl_cherrytrail_XMM_7260_ssic.cfg \
     amtl_clovertrail_XMM_6360.cfg \
     amtl_clovertrail_XMM_7160.cfg \
     amtl_merrifield_XMM_6360.cfg \
